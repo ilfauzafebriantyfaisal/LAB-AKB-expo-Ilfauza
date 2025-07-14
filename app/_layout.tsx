ee2332a242720d994df68a3f5f41983c005ebbe9
ilfauzafebriantyfaisal <105841110222@student.unismuh.ac.id>
2025-07-14 16:57:53 +0700

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font"
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+const [loaded, error] = useFonts({
+  "WinkyRough-Italic-VariableFont" :
+  require("../assets/fonts/WinkyRough-Italic-VariableFont_wght.ttf"),
+
+})
+
+  useEffect(() => {
+    if(loaded && error == null) {
+      SplashScreen.hideAsync();
+    }
+  }, [ loaded, error ])
+
+  if(!loaded && !error) {
+    return null
+  }
   return <Stack />;
+
 }
